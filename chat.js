e12432cc91212026777626f104c2f95ae429f4b9
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2023-09-21 12:23:37 +0600

    new commit

---
+++ chat.js
@@ -0,0 +1,6 @@
+// JavaScript functionality (example: changing the header text)
+const header = document.querySelector('header h1');
+
+header.addEventListener('click', function() {
+    header.textContent = 'Welcome to My Updated Demo Website';
+});
